63b947b64ce400645db0b84e8affb9e17ec5648d
ScofieldZhang <302996244@qq.com>
2021-12-01 10:53:09 +0800

    second commit

--- app/src/main/java/com/inmoglass/appModule/MainActivity.kt
+++ app/src/main/java/com/inmoglass/appModule/MainActivity.kt
@@ -3,9 +3,9 @@ package com.inmoglass.appModule
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
     }
 }
